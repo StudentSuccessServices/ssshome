29ceecb418e7b46e89ac8e3a9d80ef827d4d5b62
brucearctor <5032356+brucearctor@users.noreply.github.com>
2024-11-13 11:46:58 -0800

    first

---
+++ lib/src/homepage.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // title: const Text('Student Success Services'),
+      ),
+      body: const SingleChildScrollView(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Center(
+              child: Text(
+                'Welcome to Student Success Services!',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 16),
+            Center(
+              child: Text(
+                'We are here to support the academic journey, from getting started through your successful outcomes.',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            SizedBox(height: 16),
+          ],
+        ),
+      ),
+    );
+  }
+}
